cad0865503cc43af9504fa74cbc65c90f1a62a49
huuly01092003 <huuly1357@gmail.com>
2024-09-29 23:03:45 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('Welcome to the Jamstack site!');
+document.getElementById('run-button').addEventListener('click', function() {
+    const content = document.getElementById('content-area').value;
+    alert('Content entered: ' + content);
+  });
+  
